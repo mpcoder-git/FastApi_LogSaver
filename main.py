0d112c1f9adf397e8baf631a4afbd14c28eec502
mpcoder-git <mpcoder@mail.ru>
2023-03-23 14:30:08 +0600

    Корректировка неточностей
    Внесены дополнения для корректной работы скрипта согласно изменениям в таблице.
    Добавлен файл main.py, предназначенный для отладки работы скрипта.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# Отладочный модуль
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy import  Column, Integer, String, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.orm import sessionmaker
-
+import uvicorn
 
 engine = create_engine("mysql+pymysql://root:root@localhost/logsklad")
 Base = declarative_base()
 Session = sessionmaker(autoflush=False, bind=engine)
 
+
 class Logrow(Base):
     __tablename__ = "logtable"
-  
+
     Id = Column(Integer, primary_key=True, index=True)
     UserId = Column(Integer)
     LocalName = Column(String)
@@ -21,18 +23,19 @@ class Logrow(Base):
     Datetimesave = Column(DateTime)
 
 
-
 app = FastAPI()
- 
+
 @app.get("/")
 def read_root():
     html_content = "<h2>WEB SERVER LOGSAVER</h2>"
     return HTMLResponse(content=html_content)
 
+
 @app.get("/status")
 def fn_status():
     html_content = "OK"
     return HTMLResponse(content=html_content)
+
 
 @app.post("/logsave")
 def fn_logsave(useridx='', locname='', dataset='', querytext='', dtsave='0000-00-00 00:00:00'):
@@ -40,14 +43,19 @@ def fn_logsave(useridx='', locname='', dataset='', querytext='', dtsave='0000-00
         message = "Одна или несколько переменных не найдены"
         return HTMLResponse(content=message)
     else:
-        #print(querytext)
+        # print(querytext)
         message = ""
         # создаем сессию подключения к бд
         with Session(autoflush=False, bind=engine) as db:
             # создаем объект Logrow для добавления в бд
-            lrow = Logrow(UserId=useridx, LocalName=locname, Component=dataset, Querytext=querytext, Datetimesave=dtsave)
-            db.add(lrow)     # добавляем в бд
-            db.commit()     # сохраняем изменения
-            #print(lrow.id)   # можно получить установленный id
-            message = message + "insert OK № "+str(lrow.Id)
+            lrow = Logrow(UserId=useridx, LocalName=locname, Component=dataset, Querytext=querytext,
+                          Datetimesave=dtsave)
+            db.add(lrow)  # добавляем в бд
+            db.commit()  # сохраняем изменения
+            # print(lrow.id)   # можно получить установленный id
+            message = message + "insert OK № " + str(lrow.Id)
             return HTMLResponse(content=message)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
